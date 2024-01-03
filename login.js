6cd38d0a01d794638b605f56617a0cbd5141b04e
shivam <sharmashivam52465@gmail.com>
2024-01-03 09:07:45 +0530

    js commit

--- login.js
+++ login.js
@@ -1,11 +1,36 @@
 let check = document.getElementById('check');
 check.onclick = toglepass;
-function toglepass(){
-    if(check.checked)
-    {
+function toglepass() {
+    if (check.checked) {
         pass.type = 'text'
     }
-    else{
+    else {
         pass.type = 'password'
     }
 }
+var userDetails = []
+var form = document.getElementById("form")
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    var btn = document.getElementById("btn")
+    var Email = document.getElementById("Email").value
+    var pass = document.getElementById("pass").value
+    var age = document.getElementById("age").value
+    if (Email == "" || pass == "" || age == "") {
+        btn.style.backgroundColor = "red"
+        btn.textContent = "please enter details"
+    } else {
+        var inputValues = {
+            Email,
+            pass,
+            age
+        }
+        btn.style.backgroundColor = ""
+        btn.textContent = "Login"
+        userDetails.push(inputValues)
+        form.reset()
+        console.log(userDetails);
+    }
+
+
+})
